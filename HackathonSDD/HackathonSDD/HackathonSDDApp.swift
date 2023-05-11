71ebe62a963a3fa99fdec910a52d915c1f39fa6a
karunpant-pcln <karun.pant@priceline.com>
2023-05-11 14:44:43 +0530

    Hackathin ios project CI repo

---
+++ HackathonSDD/HackathonSDD/HackathonSDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonSDDApp.swift
+//  HackathonSDD
+//
+//  Created by Karun Pant on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonSDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
